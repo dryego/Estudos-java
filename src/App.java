973b58ed1392a1ae9ccfcc8a671257b3b1ce72fc
dryego barbosa <dryegobarbosa@MacBook-Air-de-dryego.local>
2023-12-06 13:28:34 -0300

    variaveis

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, mundo!!");
+
+        double salario = 4000;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroCurto;
+        String textoNumero = "Um";
+
+        System.out.println(salario);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto2);
+        System.out.println(textoNumero);
     }
 }
