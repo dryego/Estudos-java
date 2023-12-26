c5a107f940a40c8bb205bb2e56c2afacbb7cc795
dryego barbosa <dryegobarbosa@MacBook-Air-de-dryego.local>
2023-12-26 14:48:42 -0300

    Equals

--- src/App.java
+++ src/App.java
@@ -2,15 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, mundo!!");
 
+        String nomeUm = "dryego";
+        String nomeDois = new String("dryego");
+
         double salario = 4000;
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = numeroCurto;
-        String textoNumero = "Um";
+        final String TEXTO_NUMERO = "Um";
 
         System.out.println(salario);
         System.out.println(numeroNormal);
         System.out.println(numeroCurto2);
-        System.out.println(textoNumero);
+        System.out.println(TEXTO_NUMERO);
+
+        System.out.println(nomeUm.equals(nomeDois));
     }
 }
